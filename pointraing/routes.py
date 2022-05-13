242b82696ce56a5c7fed73c86c8342ed55b30dcc
l.e.nigmatullina <myueltwo@gmail.com>
2022-05-13 16:29:00 +0500

    adding template layout

--- pointraing/routes.py
+++ pointraing/routes.py
@@ -1,5 +1,7 @@
 from pointraing import app
+from flask import render_template, url_for
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
