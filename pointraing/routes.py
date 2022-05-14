4aa19379ce4159988f875cb38385f75d6d3b5de6
l.e.nigmatullina <myueltwo@gmail.com>
2022-05-14 16:39:58 +0500

    page about

--- pointraing/routes.py
+++ pointraing/routes.py
@@ -13,6 +13,11 @@ def home():
     return render_template('home.html')
 
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
