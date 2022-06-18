e5eb8057842396a26048c4465a3cb5a0bee22058
l.e.nigmatullina <myueltwo@gmail.com>
2022-06-18 16:45:26 +0500

    MYSQL database

--- pointraing/students/forms.py
+++ pointraing/students/forms.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileRequired, FileField
+from flask_wtf.file import FileAllowed, FileRequired, FileField, FileSize
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
 class StudentActivityForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
-    sub_type_id = SelectField('Тип')
-    file = FileField('Загрузите грамоту/сертификат', validators=[FileAllowed(['jpg', 'png', 'pdf']), FileRequired()])
+    sub_type_id = SelectField('Тип', validate_choice=False)
+    file = FileField('Загрузите грамоту/сертификат',
+                     validators=[
+                         FileAllowed(['jpg', 'png', 'pdf']),
+                         FileRequired(),
+                         FileSize(500000)
+                     ])
     submit = SubmitField('Отправить')
