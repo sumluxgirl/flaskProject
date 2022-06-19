803193f4ced723e6c0776309ecc42debdf6f106b
l.e.nigmatullina <myueltwo@gmail.com>
2022-06-19 23:25:08 +0500

    backward compatibility

--- pointraing/__init__.py
+++ pointraing/__init__.py
@@ -14,8 +14,8 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:L500666l@localhost/point_rating'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:L500666l@localhost/point_rating'
     UPLOAD_FOLDER = os.path.join(app.root_path, 'static/activity_files')
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     db.init_app(app)
